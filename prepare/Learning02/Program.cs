2b9b6984e44f9f35a617d9ec9f23c7dbbb3d8935
braquelw <appaloosarider7@gmail.com>
2023-01-06 15:34:10 -0700

    new learning 2 program

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Amazon Account Manager";
+        job1._company = "Komboventures";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Account Specialist";
+        job2._company = "Nutraceutical";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Braquel Woodland";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
