b764bec77550ba6ad86e04a56cf4fc2e08dac939
braquelw <appaloosarider7@gmail.com>
2023-01-31 15:19:53 -0700

    added learning 5 project

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3.35);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Blue", 4, 5.25);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Yellow", 2.5);
+        shapes.Add(c1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
